18be635ab582df87aa47207d49a27c01792ab64e
Talat <talcet91@gmail.com>
2016-11-06 12:34:30 +0100

    stuff

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //React components
-import Header from "./components/Header.js"; 
+import Header from "./components/Header.js";
 
 //Styles
 import "./scss/main.scss";
@@ -11,13 +11,36 @@ import "./scss/main.scss";
 function Image(props) {
 
     return (
-        <a className="image-link" href="#">
+        <a className="image-link column-4" href="#">
             <div className="inner-wrapper">
                 <img src={props.url} alt="" />
             </div>
         </a>
     );
 }
+Image.propTypes = {
+    url : React.PropTypes.string.isRequired
+};
+
+
+var Gallery = React.createClass({
+    propTypes : {
+        photos : React.PropTypes.array.isRequired
+    },
+    render : function() {
+        return (
+            <div className="image-gallery">
+                <div className="grid-container">
+                    {this.props.photos.map(function(photo, index) {
+                        return (
+                            <Image url= {"https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg"} key={index} />
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    }
+});
 
 var Application = React.createClass({
 
@@ -32,7 +55,7 @@ var Application = React.createClass({
 
         var xhr = new XMLHttpRequest();
         var flickrApiKey = "1020fa4e42882ee2e1a144a48e55499d";
-        var flickrAPI = "https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=superhero&per_page=10&api_key=" + flickrApiKey + "&format=json&nojsoncallback=1";
+        var flickrAPI = "https://api.flickr.com/services/rest/?method=flickr.photos.search&safe_search=1&tags=batman&per_page=10&api_key=" + flickrApiKey + "&format=json&nojsoncallback=1";
 
         xhr.onreadystatechange = function() {
             if (xhr.readyState === 4 && xhr.status === 200) {
@@ -50,20 +73,11 @@ var Application = React.createClass({
             <div className="site-wrapper">
                 <Header headerText="Superhero Gallery" />
                 <section className="block">
-                        <div className="image-gallery">
-                            <div className="grid-container">
-                                {this.state.photos.map(function(photo, index) {
-                                    return (
-                                        <Image url= {"https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg"} key={index} />                                    
-                                    );
-                                })}
-                            </div>
-                        </div>
+                    <Gallery photos={this.state.photos}/>
                 </section>
             </div>
         );
     }
 });
-
 
 ReactDOM.render(<Application />, document.querySelector(".container"));
